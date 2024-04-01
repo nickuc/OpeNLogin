824ef9e2bf3fcbd7368a803afc8ce0487d0987cc
Nick <nick@nickuc.com>
2024-04-01 18:02:36 -0300

    Update copyright notice

--- openlogin-bukkit/src/main/java/com/nickuc/openlogin/bukkit/command/executors/RegisterCommand.java
+++ openlogin-bukkit/src/main/java/com/nickuc/openlogin/bukkit/command/executors/RegisterCommand.java
@@ -1,8 +1,26 @@
 /*
- * This file is part of a NickUC project
+ * The MIT License (MIT)
  *
- * Copyright (c) NickUC <nickuc.com>
- * https://github.com/nickuc
+ * A practical, secure and friendly authentication plugin.
+ * Copyright © 2024 - OpenLogin Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package com.nickuc.openlogin.bukkit.command.executors;
@@ -99,7 +117,7 @@ public class RegisterCommand extends BukkitAbstractCommand {
     }
 
     private void performConsole(CommandSender sender, String lb, String[] args) {
-        if (!sender.hasPermission("openlogin.admin"))  {
+        if (!sender.hasPermission("openlogin.admin")) {
             sender.sendMessage(Messages.INSUFFICIENT_PERMISSIONS.asString());
             return;
         }
